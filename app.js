79bfbe00b732fe9b53f7e1440ea34438628d790d
Johnny Peter <johnnypeter2002@gmail.com>
2019-09-05 17:32:47 +0530

    Remove unused dependencies

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const fs = require('fs');
 const axios = require('axios');
 const bodyParser = require('body-parser');
-const querystring = require('querystring');
 
 const app = express();
 
